47e532c79d3e18b482abdff3e4f9f5ec40332fd2
andybin <srb.123@163.com>
2021-02-21 21:01:57 +0800

    add vue page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
 import Login from '../page/login'
+// import Manage from '../page/Manage'
+// import Home from '../page/Home'
 
 Vue.use(Router)
 
